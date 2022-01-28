c9d6def1a46bd9623bc5ea4b78f9a4cd9a75ab76
Yuto Ichihara <yutounwasese@gmail.com>
2022-01-28 15:57:21 +0900

    template修正

--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -1,4 +1,10 @@
 from django.views import generic
 
+from .forms import InquiryForm
+
 class IndexView(generic.TemplateView):
   template_name = "index.html"
+
+class InquiryView(generic.FormView):
+  template_name = "inqury.html"
+  form_class = InquiryForm
